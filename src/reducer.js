f1dd9c7649fbf4ec366c0231ede9337eed142139
Tanmay Daga <tanmay06daga@gmail.com>
2024-03-29 16:34:37 +0530

    removed error of const blockscope

--- src/reducer.js
+++ src/reducer.js
@@ -8,33 +8,31 @@ import {
 } from "./action";
 
 const reducer = (state, action) => {
-  let newCart;
-  let item;
   switch (action.type) {
     case CLEAR_CART:
       return { ...state, cart: new Map() };
     case REMOVE:
-      newCart = new Map(state.cart);
+      const newCart = new Map(state.cart);
       newCart.delete(action.payload.id);
       return { ...state, cart: newCart };
-    case INCREASE:
-      newCart = new Map(state.cart);
-      item = newCart.get(action.payload.id);
+    case INCREASE: {
+      const newCart = new Map(state.cart);
+      const item = newCart.get(action.payload.id);
       newCart.set(action.payload.id, { ...item, amount: item["amount"] + 1 });
       return { ...state, cart: newCart };
-
-    case DECREASE:
-      newCart = new Map(state.cart);
-      item = newCart.get(action.payload.id);
+    }
+    case DECREASE: {
+      const newCart = new Map(state.cart);
+      const item = newCart.get(action.payload.id);
       if (item["amount"] == 1) {
         return reducer(state, { ...action, type: REMOVE });
       }
       newCart.set(action.payload.id, { ...item, amount: item["amount"] - 1 });
       return { ...state, cart: newCart };
+    }
     case LOADING:
       return { ...state, loading: true };
-    case DISPLAY:
-  
+    case DISPLAY: {
       const newCart = action.payload.cart;
 
       return {
@@ -42,6 +40,7 @@ const reducer = (state, action) => {
         loading: false,
         cart: new Map(newCart.map((item) => [item.id, item])),
       };
+    }
     default:
       throw new Error(`No action type found of type - ${action.type}`);
   }
